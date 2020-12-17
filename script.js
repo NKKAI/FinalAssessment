d4b7214293e22037d4e43b2802bcbdbc0b2932d7
Andre Kim <yuenwoo.kim@myrcs.org>
2020-12-17 05:21:08 +0000

    final

---
+++ script.js
@@ -0,0 +1,11 @@
+function length() {
+  console.log("We are")
+}
+
+function alphabet(){
+  console.log("finished this")
+}
+
+function reverse alphabetical(){
+  console.log("Computer Science 20 class!")
+}
